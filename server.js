2045ca1623657c4d6af2f5b2cf1ab5e6936f9d4f
Md Hasanur Jaman <hkmdhasanurjaman82@gmail.com>
2025-11-16 15:58:35 +0600

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 🔥 Make sure BOTH lines exist
 const PUBLIC_DIR = path.join(__dirname, "public");
